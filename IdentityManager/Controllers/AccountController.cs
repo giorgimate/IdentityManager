32371883a07aefb0532a3ac0751a0386f64a195d
giorgimate <108875518+giorgimate@users.noreply.github.com>
2023-10-09 16:33:16 +0400

    add login register function

--- IdentityManager/Controllers/AccountController.cs
+++ IdentityManager/Controllers/AccountController.cs
@@ -1,10 +1,19 @@
 ﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityManager.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,6 +23,28 @@ namespace IdentityManager.Controllers
         {
             var registerViewModel = new RegisterViewModel();
             return View(registerViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
+            var result = await _userManager.CreateAsync(user, model.ConfirmPassword);
+            if (result.Succeeded)
+            {
+                //isPersistent brozeris daxurvis shemdeg darches tu ara chveni inpormacia cookshi
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+            AddErrors(result);
+            return View(model);
+        }
+        //identitize yvela erors gamoitans
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
---
+++ IdentityManager/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityManager.Models
+{
+    //axali propertis damateba useristvis , unda iyos identity useris memkvidre rom mixvdes bazis userzeromaa saubari
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
