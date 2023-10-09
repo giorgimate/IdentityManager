0f8b806ba0595a1c07d543944e37434ef013fb44
giorgimate <108875518+giorgimate@users.noreply.github.com>
2023-10-09 15:10:08 +0400

    Add project files.

---
+++ IdentityManager/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            var registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+        }
+    }
+}
